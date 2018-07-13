6083df2267cd0fe371050411629137f5b9d4ac2b
dich <34095473+idichaves@users.noreply.github.com>
2018-07-13 23:04:38 +0800

    added something

--- upload.js
+++ upload.js
@@ -55,8 +55,15 @@ $(document).ready( function() {
         $('.img-preview').attr('src', $(this).attr('src'))
     })
     $("#post").on('click',function(e){
-        e.preventDefault();
-        alert("Successfully Uploaded!");
-        location.reload(false);
+        e.preventDefault()
+        alert("Successfully Uploaded")
+        location.reload(true)
     })
 })
+
+var getQueryString = function(field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i')
+    var string = reg.exec(href);
+    return string ? string[1] : null
+}
